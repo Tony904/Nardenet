ae1850d104be0afe7e2036943dcfa4551545dc68
Tony904 <tnarde@gmail.com>
2024-05-27 14:17:53 -0400

    Delete locmacro.c

--- locmacro.c
+++
@@ -1,7 +0,0 @@
-#include "locmacro.h"
-#include <stdio.h>
-
-
-void print_location(const char* const filename, const char* const funcname, const int line) {
-	printf("LOCATION: %s, %s, line %d", filename, funcname, line);
-}
