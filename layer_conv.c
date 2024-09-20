d78a654ec228ddbbc1db94b1742b1e5943bca1dc
Tony904 <tnarde@gmail.com>
2024-09-20 08:09:25 -0400

    Add files via upload

---
+++ layer_conv.c
@@ -0,0 +1,223 @@
+#include "layer_conv.h"
+#include <omp.h>
+#include <assert.h>
+#include <string.h>
+#include "xallocs.h"
+#include "im2col.h"
+#include "gemm.h"
+#include "network.h"
+#include "activations.h"
+#include "derivatives.h"
+#include "xarrays.h"
+#include "utils.h"
+#include "batchnorm.h"
+
+
+void forward_conv(layer* l, network* net) {
+	size_t out_n = l->out_n;
+	size_t batch_size = net->batch_size;
+	zero_array(l->Z, out_n * batch_size);
+	size_t n_groups = l->n_groups;
+	size_t w = l->w;
+	size_t h = l->h;
+	size_t M = l->n_filters;
+	size_t N = l->out_w * l->out_h;
+	size_t K = l->ksize * l->ksize * l->c;
+	float* A = l->weights.a;  // M * K
+	for (size_t b = 0; b < batch_size; b++) {
+		float* B = net->workspace.a;  // K * N
+		float* B0 = B;
+		for (size_t i = 0; i < l->in_ids.n; i++) {
+			layer* inl = l->in_layers[i];
+			size_t c = inl->out_c;
+			float* im = &inl->output[b * inl->out_n];
+			im2col(im, (int)c, (int)h, (int)w, (int)l->ksize, (int)l->pad, (int)l->stride, B);
+			B += K * l->ksize * l->ksize * c;
+		}
+		float* C = &l->Z[b * M * N];  // M * N
+		if (n_groups > 1) gemm_groups(M, N, K, A, B0, C, n_groups);
+		else gemm(M, N, K, A, B0, C);
+	}
+	if (l->batch_norm) {
+		forward_batch_norm(l, batch_size);
+		l->activate(l->act_inputs, l->output, out_n, batch_size);
+	}
+	else {
+		// note: l->Z = l->act_inputs when batchnorm disabled
+		add_biases(l->Z, l->biases, M, N, batch_size);
+		l->activate(l->Z, l->output, out_n, batch_size);
+	}
+	if (net->training) zero_array(l->grads, out_n * batch_size);
+}
+
+void backward_conv(layer* l, network* net) {
+	size_t batch_size = net->batch_size;
+	float* grads = l->grads;  // propogated gradients up to this layer
+	// dz/dw = previous layer (shallower layer) input
+	// da/dz = activation derivative
+	if (l->activation == ACT_MISH) get_grads_mish(grads, l->act_inputs, l->out_n, batch_size);  // dC/da * da/dz
+	else if (l->activation == ACT_RELU) get_grads_relu(grads, l->act_inputs, l->out_n, batch_size);
+	else if (l->activation == ACT_LEAKY) get_grads_leaky_relu(grads, l->act_inputs, l->out_n, batch_size);
+	else if (l->activation == ACT_SIGMOID) get_grads_sigmoid(grads, l->output, l->out_n, batch_size);
+	else if (l->activation == ACT_SOFTMAX);
+	else if (l->activation == ACT_TANH) get_grads_tanh(grads, l->act_inputs, l->out_n, batch_size);
+	else if (l->activation == ACT_NONE);
+	else {
+		printf("Incorrect or unsupported activation function.\n");
+		wait_for_key_then_exit();
+	}
+	// grads[] is now dC/dz.
+	
+	// now get dz/dw for each weight (it's just the input from the previous layer in the forward pass).
+	size_t M = l->n_filters;
+	size_t N = l->ksize * l->ksize * l->c; // filter length
+	size_t K = l->out_w * l->out_h; // # of patches
+
+	// sum dC/dz for each filter to get it's bias gradients.
+	get_bias_grads(l->bias_grads, grads, M, K, batch_size);  // note: biases = betas for batch norm
+
+	if (l->batch_norm) backward_batch_norm(l, batch_size);
+
+	size_t w = l->w;
+	size_t h = l->h;
+	for (size_t s = 0; s < batch_size; s++) {
+		float* A = &grads[s * M * K];  // M * K
+		float* B = net->workspace.a;  // N * K
+		zero_array(B, (size_t)(N * K));
+		float* B0 = B;
+		float* C = l->weight_grads;  // M * N
+		for (size_t i = 0; i < l->in_ids.n; i++) {
+			layer* inl = l->in_layers[i];
+			size_t c = inl->out_c;
+			float* im = &inl->output[s * inl->out_n];
+			im2col(im, (int)c, (int)h, (int)w, (int)l->ksize, (int)l->pad, (int)l->stride, B);
+			B += N * l->ksize * l->ksize * c;
+		}
+		B = B0;
+		gemm_atb(M, N, K, A, B, C);
+	}
+	// C is now dC/dw for all weights. 
+	// Note: C array's storage structure is [filter_index * filter_length + filter_weight_index]
+	
+	// Now need to create backpropogated "image" for shallower layer(s),
+	// so we need to calculate dz/da (dz of this layer wrt da of input layer(s)),
+	// which is just the weights. (dz/da = weights of current layer)
+	// and then multiply that by dC/dz (which is currently the grads array).
+	// Note: Weight gradients DO NOT propagate back, they are just used to update the weights.
+
+	if (l->id == 0) return;
+	for (size_t s = 0; s < batch_size; s++) {
+		float* A = l->weights.a;  // M * N
+		float* B = &grads[s * M * K];  // M * K
+		float* C = net->workspace.a;  // N * K
+		zero_array(C, (size_t)(N * K));
+		gemm_tab(M, N, K, A, B, C);
+		// C is now dC/da in col'd form (as in im2col).
+		// So now we need to turn this "expanded" form (col) into the form of the dimensions of
+		// the output of the input layer (im). We do this using col2im().
+
+		for (size_t i = 0; i < l->in_ids.n; i++) {
+			layer* inl = l->in_layers[i];
+			size_t c = inl->out_c;
+			float* im = &inl->grads[s * w * h * c];
+			col2im(C, (int)c, (int)h, (int)w, (int)l->ksize, (int)l->pad, (int)l->stride, im);
+		}
+	}
+}
+
+void update_conv(layer* l, network* net) {
+	float rate = net->current_learning_rate;	
+	float momentum = net->momentum;
+
+	float* biases = l->biases;
+	float* bias_grads = l->bias_grads;
+	float* biases_velocity = l->biases_velocity;
+	size_t n = l->n_filters;
+	size_t i;
+#pragma omp parallel for firstprivate(rate, momentum)
+	for (i = 0; i < n; i++) {
+		float v_old = biases_velocity[i];
+		float v_new = momentum * v_old - rate * bias_grads[i];
+		biases[i] += -momentum * v_old + (1 + momentum) * v_new;  // Nesterov momentum
+		biases_velocity[i] = v_new;
+		bias_grads[i] = 0.0F;
+	}
+
+	float* weights = l->weights.a;
+	float* weight_grads = l->weight_grads;
+	float* weights_velocity = l->weights_velocity;
+	n = l->weights.n;
+	net->regularize_weights(weight_grads, weights, n, net->decay);
+#pragma omp parallel for firstprivate(rate, momentum)
+	for (i = 0; i < n; i++) {
+		float v_old = weights_velocity[i];
+		float v_new = momentum * v_old - rate * weight_grads[i];
+		weights[i] += -momentum * v_old + (1 + momentum) * v_new;
+		weights_velocity[i] = v_new;
+		weight_grads[i] = 0.0F;
+	}
+
+	if (l->batch_norm) {
+		float* gammas = l->gammas;
+		float* gamma_grads = l->gamma_grads;
+		float* gammas_velocity = l->gammas_velocity;
+		n = l->out_c;
+#pragma omp parallel for firstprivate(rate, momentum)
+		for (i = 0; i < n; i++) {
+			float v_old = weights_velocity[i];
+			float v_new = momentum * v_old - rate * weight_grads[i];
+			gammas[i] += -momentum * v_old + (1 + momentum) * v_new;
+			gammas_velocity[i] = v_new;
+			gamma_grads[i] = 0.0F;
+		}
+	}
+}
+
+/*** TESTS ***/
+
+void test_forward_conv(void) {
+	layer* l = (layer*)xcalloc(1, sizeof(layer));
+	network* net = (network*)xcalloc(1, sizeof(network));
+	l->n_filters = 2;
+	l->ksize = 2;
+	l->pad = 0;
+	l->stride = 1;
+	l->w = 3;
+	l->h = 3;
+	l->c = 3;
+	l->out_w = (l->w + 2 * l->pad - l->ksize) / l->stride + 1;
+	l->out_h = (l->h + 2 * l->pad - l->ksize) / l->stride + 1;
+	l->out_c = l->n_filters;
+	l->biases = (float*)xcalloc(l->n_filters, sizeof(float));
+	fill_array(l->biases, l->n_filters, 0.5F);
+	l->activate = activate_none;
+	l->weights.n = l->n_filters * l->ksize * l->ksize * l->c;
+	l->weights.a = (float*)xcalloc(l->weights.n, sizeof(float));
+	fill_array(l->weights.a, l->weights.n, 1.0F);
+	l->Z = (float*)xcalloc(l->n_filters * l->out_w * l->out_h, sizeof(float));
+	layer* inl1 = (layer*)xcalloc(1, sizeof(layer));
+	layer* inl2 = (layer*)xcalloc(1, sizeof(layer));
+	inl1->out_w = l->w;
+	inl2->out_w = l->w;
+	inl1->out_h = l->h;
+	inl2->out_h = l->h;
+	inl1->out_c = 1;
+	inl2->out_c = 2;
+	assert(l->c == inl1->out_c + inl2->out_c);
+	inl1->out_n = inl1->out_w * inl1->out_h * inl1->out_c;
+	inl2->out_n = inl2->out_w * inl2->out_h * inl2->out_c;
+	inl1->output = (float*)xcalloc(inl1->out_n, sizeof(float));
+	fill_array(inl1->output, inl1->out_n, 2.0F);
+	inl2->output = (float*)xcalloc(inl2->out_n, sizeof(float));
+	fill_array(inl2->output, inl2->out_n, 1.0F);
+	l->in_layers = (layer**)xcalloc(2, sizeof(layer*));
+	l->in_layers[0] = inl1;
+	l->in_layers[1] = inl2;
+	l->in_ids.n = 2;
+
+	net->workspace.a = (float*)xcalloc(l->out_w * l->out_h * l->ksize * l->ksize * l->c * 2, sizeof(float));
+
+	forward_conv(l, net);
+
+	pprint_mat(l->Z, (int)l->out_w, (int)l->out_h, (int)l->out_c);
+}
---
+++ network.h
@@ -0,0 +1,287 @@
+#ifndef NETWORK_H
+#define NETWORK_H
+
+#include <stdlib.h>
+#include "xarrays.h"
+
+#define NUM_ANCHOR_PARAMS 5  // probability contains any object, center_x, center_y, width, height
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    typedef struct network network;
+    typedef struct layer layer;
+    typedef struct class_set class_set;
+    typedef struct classifier_dataset classifier_dataset;
+    typedef struct bbox bbox;
+    typedef struct det_cell det_cell;
+    typedef struct det_sample det_sample;
+    typedef struct detector_dataset detector_dataset;
+    typedef enum LR_POLICY LR_POLICY;
+    typedef enum LAYER_TYPE LAYER_TYPE;
+    typedef enum ACTIVATION ACTIVATION;
+    typedef enum LOSS_TYPE LOSS_TYPE;
+    typedef enum REGULARIZATION REGULARIZATION;
+    typedef enum NET_TYPE NET_TYPE;
+
+
+    network* new_network(size_t num_of_layers);
+    void build_network(network* net);
+    void backward_none(layer* l, network* net);
+    void update_none(layer* l, network* net);
+
+    void free_network(network* net);
+
+    void print_layers(network* net);
+    void print_layer(layer* l);
+    void print_network(network* net);
+    void print_lrpolicy(LR_POLICY lrp);
+    void print_layertype(LAYER_TYPE lt);
+    void print_activation(ACTIVATION a);
+    void print_loss_type(LOSS_TYPE c);
+    void print_all_network_weights(network* net);
+    void print_some_weights(layer* l, size_t n);
+    void print_top_class_name(float* probs, size_t n_classes, char** class_names, int include_prob, int new_line);
+    void print_network_summary(network* net, int print_training_params);
+    void print_network_structure(network* net);
+    void print_prediction_results(network* net, layer* prediction_layer);
+
+    typedef struct classifier_dataset {
+        class_set* sets;
+        size_t n;       // # of sets/classes
+        size_t* rands;  // array of non-repeating random numbers of size n
+        size_t ri;      // rands index
+    } classifier_dataset;
+
+    typedef struct detector_dataset {
+        det_sample* samples;
+        size_t n;       // # of samples
+        size_t* rands;  // array of non-repeating random numbers of size n
+        size_t ri;      // rands index
+        det_sample** current_batch;
+    } detector_dataset;
+
+    typedef struct network {
+        size_t n_layers;
+        size_t w;
+        size_t h;
+        size_t c;
+        size_t n_classes;
+        char** class_names;
+        int training;  // 1 = training, 0 = inference only
+        size_t batch_size;  // number of images per batch
+        size_t subbatch_size;  // number of images per sub-batch, must divide evenly into batch_size
+        size_t max_iterations;  // maximum number of training iterations before automatically ending training
+        size_t save_frequency;
+
+        float learning_rate;
+        float current_learning_rate;
+        LR_POLICY lr_policy;
+        floatarr step_percents;
+        floatarr step_scaling;
+        size_t ease_in;
+        float momentum;
+
+        REGULARIZATION regularization;
+        void(*reg_loss)             (network*);
+        void(*regularize_weights)   (float*, float*, size_t, float);
+        int batch_norm;  // 1 if any layer has batch norm enabled
+        float loss;
+        float decay;
+        size_t iteration;  // current iteration, one iteration = one batch processed
+        size_t epoch_size;  // number of batches to complete one epoch
+
+        float saturation[2];
+        float exposure[2];
+        float hue[2];
+
+        layer* input;
+        layer* layers;
+        floatarr workspace;
+
+        char* cfg_file;
+        char* dataset_dir;
+        char* weights_file;
+        char* save_dir;
+        NET_TYPE type;
+        union data {
+            classifier_dataset clsr;
+            detector_dataset detr;
+        } data;
+
+        bbox* anchors;  // anchors across all detection layers
+        size_t n_anchors;
+
+        float draw_thresh;  // threshold for drawing detections (if p >= draw_thresh then draw)
+    } network;
+
+    typedef struct layer {
+        int id;
+        LAYER_TYPE type;
+        ACTIVATION activation;
+        void(*activate)  (float*, float*, size_t, size_t);
+        void(*forward)   (layer*, network*);
+        void(*backward)  (layer*, network*);
+        void(*update)    (layer*, network*);
+        void(*get_loss)  (layer*, network*);
+        size_t n_groups;
+        size_t n_filters;
+        size_t ksize;
+        size_t stride;
+        size_t pad;
+        size_t w;
+        size_t h;
+        size_t c;
+        size_t n; // n_inputs = w * h * c
+        size_t out_w; // out_w = ((w + (pad * 2) - ksize) / stride) + 1
+        size_t out_h; // out_h = ((h + (pad * 2) - ksize) / stride) + 1
+        size_t out_c; // out_c = n_filters
+        size_t out_n; // out_n = out_w * out_h * out_c
+        float* output;
+
+        floatarr weights;  // n_weights = ksize * ksize * c * n_filters
+        float* biases;
+        float* act_inputs;  // activation function inputs
+
+        int batch_norm;
+        float* Z;  // results of weights * inputs (when batchnorm enabled)
+        float* Z_norm;  // Normalized Z
+        float* means;
+        float* variances;
+        float* gammas;  // normalized values scales
+        float* rolling_means;
+        float* rolling_variances;
+        float* mean_grads;
+        float* variance_grads;
+        float* gamma_grads;
+        float* gammas_velocity;
+
+        float* errors;
+        float* grads;  // storage for propagated gradients
+        float* weight_grads;
+        float* bias_grads;
+        float* weights_velocity;  // momentum adjustment for weights
+        float* biases_velocity;  // momentum adjustment for biases
+
+        intarr in_ids;
+        intarr out_ids;
+        layer** in_layers;
+
+        LOSS_TYPE loss_type;
+        float loss;
+        float obj_loss;
+        float cls_loss;
+        float iou_loss;
+
+        int train;
+
+        size_t n_classes;
+        size_t n_anchors;
+        bbox* anchors;  // base anchors that will get copied to each cell
+        bbox* detections;
+        bbox** sorted;
+        float nms_obj_thresh;
+        float nms_cls_thresh;
+        float nms_iou_thresh;
+        float* truth;  // truths for classifier
+
+        float ignore_thresh;
+        float iou_thresh;
+        float obj_normalizer;
+        float max_box_grad;
+        float scale_grid;
+
+        float** maxpool_addresses;  // addresses of input layer outputs that were max values (for backprop)
+    } layer;
+
+    typedef enum NET_TYPE {
+        NET_NONE,
+        NET_CLASSIFY,
+        NET_DETECT
+    } NET_TYPE;
+
+    typedef enum LAYER_TYPE {
+        LAYER_INPUT = -1,
+        LAYER_NONE,
+        LAYER_CONV,
+        LAYER_MAXPOOL,
+        LAYER_FC,
+        LAYER_CLASSIFY,
+        LAYER_RESIDUAL,
+        LAYER_DETECT,
+        LAYER_AVGPOOL,
+        LAYER_UPSAMPLE
+    } LAYER_TYPE;
+
+    typedef enum LR_POLICY {
+        LR_STEPS
+    } LR_POLICY;
+
+    typedef enum ACTIVATION {
+        ACT_NONE,
+        ACT_RELU,
+        ACT_LEAKY,
+        ACT_MISH,
+        ACT_SIGMOID,
+        ACT_SOFTMAX,
+        ACT_TANH
+    } ACTIVATION;
+
+    typedef enum LOSS_TYPE {
+        LOSS_NONE,
+        LOSS_MAE,   // mean absolute error
+        LOSS_MSE,   // mean squared error
+        LOSS_CCE,   // categorical cross-entropy, used for softmax loss
+        LOSS_BCE    // binary cross-entropy, used for sigmoid loss
+    } LOSS_TYPE;
+
+    typedef enum REGULARIZATION {
+        REG_NONE,
+        REG_L1,     // sum of absolute values of weights
+        REG_L2      // sum of squared values of weights (much preferred over L1)
+    } REGULARIZATION;
+
+    typedef struct bbox {
+        int lbl;
+        float prob;  // probability
+        // values are relative (may change idk)
+        float cx;
+        float cy;
+        float w;
+        float h;
+        float left;
+        float right;
+        float top;
+        float bottom;
+        float area;
+    } bbox;
+
+    typedef struct det_cell {   // cell of a detection layer used for training
+        float top;              // percentage of image width
+        float left;
+        float bottom;
+        float right;
+        bbox* tboxes;           // ground truth bounding boxes
+        int* obj;               // ground truth objectness in anchors
+        int* cls;               // ground truth class id in anchors
+    } det_cell;
+
+    typedef struct det_sample { // object detection sample
+        size_t n;               // # of bboxes
+        bbox* bboxes;
+        char* imgpath;
+    } det_sample;
+
+    typedef struct class_set {  // classifier class dataset
+        char** files;
+        size_t class_id;
+        size_t n;       // # of files
+        size_t* rands;  // array of non-repeating random numbers of size n
+        size_t ri;      // rands index
+    } class_set;
+
+#ifdef __cplusplus
+}
+#endif
+#endif
