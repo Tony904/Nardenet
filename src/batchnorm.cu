5586c09633efa1af1b254f5c143b236a0208cd42
Tony904 <tnarde@gmail.com>
2025-05-20 18:40:00 -0400

    Add files via upload

--- src/batchnorm.cu
+++ src/batchnorm.cu
@@ -27,23 +27,23 @@ __global__ void forward_batchnorm_kernel(float* Z, int batch_size, int n_filters
 
 }
 
-__global__ void batchnorm_kernel(float* variances, float* means, float* Z, int spatial, int batch_size, int out_n) {
+__global__ void batchnorm_kernel(float* gammas, float* betas, float* means, float* variances, float* rolling_means, float* rolling_variances, float* Z, float* Z_norm, float* act_inputs, int spatial, int batch_size, int out_n) {
 
-	__shared__ extern float shared_mem[];
-
+	__shared__ extern float shared[];
+	
 	int tid = threadIdx.x;
 	int filter = blockIdx.x;
 	int block_size = blockDim.x;
 
-	// Calculate means
-	shared_mem[tid] = 0.0F;
+	int fst = filter * spatial + tid;
 
+	// Calculate means
+	shared[tid] = 0.0F;
 	// Copy/Add data to shared memory
 	for (int b = 0; b < batch_size; b++) {
+		int offset = b * out_n + fst;
 		for (int s = 0; s < spatial; s += block_size) {
-			int z = b * out_n + filter * spatial + s + tid;
-			if (s + tid < spatial) shared_mem[tid] += 0.0F;
-			else shared_mem[tid] += Z[z];
+			if (s + tid < spatial) shared[tid] += Z[offset + s];
 		}
 	}
 	__syncthreads();
@@ -51,24 +51,23 @@ __global__ void batchnorm_kernel(float* variances, float* means, float* Z, int s
 	// Parallel reduction sum
 	for (int stride = block_size / 2; stride > 0; stride >>= 1) {
 		if (tid < stride) {
-			shared_mem[tid] += shared_mem[tid + stride];
+			shared[tid] += shared[tid + stride];
 		}
 		__syncthreads();
 	}
 
+	float mean = shared[0] / (float)(spatial * batch_size);
 	if (tid == 0) {
-		shared_mem[0] /= (float)(spatial * batch_size);
-		means[filter] = shared_mem[0];
+		means[filter] = mean;
+		rolling_means[filter] = (mean * 0.01F) + (rolling_means[filter] * 0.99F);
 	}
 
 	// Calculate variances
-	float mean = shared_mem[0];
-	shared_mem[tid] = 0.0F;
+	shared[tid] = 0.0F;
 	for (int b = 0; b < batch_size; b++) {
+		int offset = b * out_n + fst;
 		for (int s = 0; s < spatial; s += block_size) {
-			int z = b * out_n + filter * spatial + s + tid;
-			if (s + tid < spatial) shared_mem[tid] += 0.0F;
-			else shared_mem[tid] += powf(Z[z] - mean, 2.0F);
+			if (s + tid < spatial) shared[tid] += powf(Z[offset + s] - mean, 2.0F);
 		}
 	}
 	__syncthreads();
@@ -76,14 +75,31 @@ __global__ void batchnorm_kernel(float* variances, float* means, float* Z, int s
 	// Parallel reduction sum
 	for (int stride = block_size / 2; stride > 0; stride >>= 1) {
 		if (tid < stride) {
-			shared_mem[tid] += shared_mem[tid + stride];
+			shared[tid] += shared[tid + stride];
 		}
 		__syncthreads();
 	}
 
+	float variance = shared[0] / (float)(spatial * batch_size);
 	if (tid == 0) {
-		shared_mem[0] /= (float)(spatial * batch_size);
-		variances[filter] = shared_mem[0];
+		variances[filter] = variance;
+		rolling_variances[filter] = (variance * 0.01F) + (rolling_variances[filter] * 0.99F);
+	}
+
+	// Normalize values
+	float gamma = gammas[filter];
+	float beta = betas[filter];
+	float sddev = sqrtf(variance + 0.00001F);
+	for (int b = 0; b < batch_size; b++) {
+		int offset = b * out_n + fst;
+		for (int s = 0; s < spatial; s += block_size) {
+			int z = offset + s;
+			if (s + tid < spatial) {
+				float znorm = (Z[z] - mean) / sddev;
+				Z_norm[z] = znorm;
+				act_inputs[z] = znorm * gamma + beta;
+			}
+		}
 	}
 }
 
@@ -109,5 +125,5 @@ void test_forward_batchnorm_gpu(layer* l, size_t batch_size) {
 	int grid_size = (int)F;
 	int block_size = 512;
 	int shared_mem_size = (int)(S * B) * sizeof(float);
-	batchnorm_kernel KARGS(grid_size, block_size, shared_mem_size) (variances, means, d_Z, (int)S, (int)B, (int)out_n);
+	batchnorm_kernel KARGS(grid_size, block_size, shared_mem_size) (gammas, betas, means, variances, rolling_means, rolling_variances, Z, Z_norm, act_inputs, S, batch_size, out_n);
 }
